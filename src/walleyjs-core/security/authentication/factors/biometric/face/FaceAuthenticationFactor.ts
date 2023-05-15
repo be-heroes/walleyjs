5357ae3eb5472b78ee8671613c4c87c2c40f2cc4
ZaradarBH <tobias.andersen79@gmail.com>
2023-05-15 08:18:04 +0200

    added comment

--- src/walleyjs-core/security/authentication/factors/biometric/face/FaceAuthenticationFactor.ts
+++ src/walleyjs-core/security/authentication/factors/biometric/face/FaceAuthenticationFactor.ts
@@ -2,6 +2,7 @@
 import {BiometricAuthenticationCredentials} from "../BiometricAuthenticationCredentials";
 import * as Symbols from "../../Symbols";
 
+//TODO: Check out https://learn.microsoft.com/en-in/azure/cognitive-services/computer-vision/overview-identity
 export class FaceAuthenticationFactor extends BiometricFactor {
     constructor() {
         super(Symbols.faceFactor);
